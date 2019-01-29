620db7f2ad7f49f91032663f37a0c6d9ae3b09de
cccyb <chenyubo1996@gmail.com>
2019-01-30 00:21:58 +0800

    feat: 主题配置项

--- app/service/common.js
+++ app/service/common.js
@@ -6,14 +6,14 @@ class CommonService extends Service {
   // 获取header和footer数据
   async getCommonData() {
     const site = await this.service.config.findConfigByGroup('site');
+    const theme = await this.service.config.findConfigByGroup('theme');
     const pages = await this.service.content.findAllPages();
     const recentPosts = await this.service.content.findRecentPosts(8);
     // const recentComments = await this.service.comment.findRecentComments(8);
-    const social = await this.service.config.findConfigByGroup('social');
     const data = {
       site,
+      theme,
       pages,
-      social,
       year: new Date().getFullYear(),
       recentPosts,
       // recentComments,
