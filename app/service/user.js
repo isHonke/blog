8d6031cd0da0d39c602f21a501ce5ddec8762e77
cccyb <chenyubo1996@gmail.com>
2019-01-30 21:28:21 +0800

    fix: 去除多余console

--- app/service/user.js
+++ app/service/user.js
@@ -6,7 +6,6 @@ class UserService extends Service {
   async find() {
     // 假如 我们拿到用户 id 从数据库获取用户详细信息
     const user = await this.app.mysql.get('user', { uid: 1 });
-    console.log(this.app.mysql);
     return { user };
   }
 }
