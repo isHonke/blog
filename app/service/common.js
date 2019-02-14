caa43643f848a3d7ebd8b46ccb80f963e2c73064
cccyb <chenyubo1996@gmail.com>
2019-02-14 15:22:17 +0800

    feat: 配置文件使用本地文件

--- app/service/common.js
+++ app/service/common.js
@@ -3,14 +3,16 @@
 const Service = require('egg').Service;
 
 const func = require('../utils/function.js');
+const yaml2obj = require('../utils/yaml2obj.js');
 
 class CommonService extends Service {
   // 获取header和footer数据
   async getCommonData() {
-    const site = await this.service.config.findConfigByGroup('site');
-    const theme = await this.service.config.findConfigByGroup('theme');
+    const prefix = this.app.config.baseDir + '/app/config/';
+    const site = yaml2obj(prefix + 'site.yml');
+    const theme = yaml2obj(prefix + 'theme.yml');
     const pages = await this.service.content.findAllPages();
-    const recentPosts = await this.service.content.findRecentPosts(8);
+    const recentPosts = await this.service.content.findRecentPosts(theme.recentlyPosts.limit);
     // const recentComments = await this.service.comment.findRecentComments(8);
     const data = {
       site,
